0f54d95bf5c2fef83da74b7e742330b6286a7936
Dinh <81209378+vudinhso@users.noreply.github.com>
2021-04-02 09:21:10 +0300

    Create FK_4DOF_screw.m

---
+++ FK_4DOF_screw.m
@@ -0,0 +1,32 @@
+clc; clear all; close all; 
+format compact; format shortG
+
+#Manipulator parameters
+D1 = .2755;
+D2 = .41;
+D3 = .2073;
+D4 = .16;
+
+#Manipulator home position
+M = [   0,1,0,0; ...
+        1,0,0,-D2;...
+        0,0,-1,D1-D3-D4;...
+        0,0,0,1];
+
+#Manipulator screw axis
+screw = [   0,0,1,0,0,0;...
+            1,0,0,0,D1,0;...
+            1,0,0,0,D1,D2;...
+            0,0,1,-D2,0,0]';
+
+#Manipulator joint axis
+th = [0,pi/2,0,0];
+
+#FK
+tic()
+T = eye(4,4);
+for i=1:length(th)
+    T = T*MatrixExp6(VecTose3(screw(:,i)*th(i)));
+end
+T = T*M
+delta_time = toc()
