a1f379ddf5183312749adb73b1dc4140f50b304c
Dinh <81209378+vudinhso@users.noreply.github.com>
2021-04-02 10:49:05 +0300

    Create IK_6DOF_curv_num.m

---
+++ IK_6DOF_curv_num.m
@@ -0,0 +1,62 @@
+clc; clear all; close all; 
+format compact; format shortG
+
+#Manipulator parameters
+D1 = .2755;
+D2 = .41;
+D3 = .2073;
+D4 = .0741;
+D5 = .0741;
+D6 = .16;
+c30 = cosd(30);
+s30 = sind(30);
+d  = D4/2/c30;
+
+#Manipulator home position
+M = [   -s30, 0,c30,D4*s30+D5+D6*c30; ...
+         0,-1,0,    -D2;...
+         c30,0,s30, D1-D3-D4*c30+D6*s30;...
+         0,0,0,  1];
+
+#Manipulator screw axis
+w5 = [s30,0,-c30];
+w6 = [c30,0, s30];
+q5 = [0, -D2, D1-D3-d];
+q6 = M(1:3,4)';
+screw = [   0,0,1,      0,0,0;...
+            1,0,0,      0,D1,0;...
+            1,0,0,      0,D1,D2;...
+            0,0,1,      -D2,0,0;...
+            w5,         cross(w5, -q5);...
+            w6,         cross(w6, -q6)]';
+
+#Manipulator desired configuration
+Td= [   -s30, 0, c30,   D4*s30+D5+D6*c30;
+        c30, 0, s30,    -D2-D3-D4*c30+D6*s30; 
+        0, 1, 0,        D1; 
+        0,0,0,1]
+
+#Manipulator initial joint axis and configuration
+#th = [0,pi/2,0,0];
+th = [0,0,0,0];
+th = rand(1,6);
+
+
+#IK
+i=0;
+err_w = 10;
+
+while i<30 && err_w>1e-6
+    T       = FKinSpace(M,screw, th);   # FK of the current config. 
+    T_bd    = TransInv(T)*Td;           # Configuration to minimise
+    ss_Vb   = MatrixLog6(T_bd);         # Obtention of the se3 matrix
+    Vb      = se3ToVec(ss_Vb);          # Obtention of the twist
+    Vs      = Adjoint(T)*Vb;            # Convert in {s} frame
+    err_w   = norm(Vs(1:3))             # Value of th for the twist to minimize
+    J       = JacobianSpace(screw, th); # Jacobian in {s} frame
+    th      = th + J\Vs;                # Update of th. 
+    i=i+1
+end
+Td
+T
+norm(Td-T)
